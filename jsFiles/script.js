2e88125035287892b0e3e4b2d469c091846b7f1e
Okekeifeanyi <okekeifeanyi20@yahoo.com>
2024-10-09 11:26:18 +0100

    landing page

---
+++ jsFiles/script.js
@@ -0,0 +1,21 @@
+// let currentIndex = 0; // Keeps track of the current index
+// const slides = document.querySelectorAll('.slide'); // Select all slide elements
+// const totalSlides = slides.length; // Total number of slides
+
+// function changeSlide(direction) {
+ 
+//     currentIndex += direction;
+
+  
+//     if (currentIndex < 0) {
+//         currentIndex = 0; 
+//     }
+//     if (currentIndex > totalSlides - 3) {
+//         currentIndex = totalSlides - 3; 
+//     }
+
+//     const offset = -currentIndex * (100 / 3);
+//     document.querySelector('.slider').style.transform = `translateX(${offset}%)`;
+// }
+
+// changeSlide(0);
